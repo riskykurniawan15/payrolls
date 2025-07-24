884c33aeedc22a58c7d2646a795e541e108c540c
Risky Kurniawan <>
2025-07-24 09:30:42 +0700

    update scheme attendances

--- database/migrations/20250723100934_create_attendances_table.down.sql
+++ database/migrations/20250723100934_create_attendances_table.down.sql
@@ -1,1 +1,10 @@
+-- Drop trigger
+DROP TRIGGER IF EXISTS update_attendances_updated_columns ON attendances;
+
+-- Drop indexes
+DROP INDEX IF EXISTS idx_attendances_user_id;
+DROP INDEX IF EXISTS idx_attendances_check_out_date;
+DROP INDEX IF EXISTS idx_attendances_check_in_date;
+
+-- Drop table
 DROP TABLE IF EXISTS attendances;
